4557ab084528d951f9fa80e9e8f978db72e2b082
Robert Gibboni <robertg@berkeley.edu>
2013-06-10 16:30:38 -0700

    temp

--- Gap.py
+++ Gap.py
@@ -5,11 +5,13 @@ import pandas as pd
 import datetime
 import misc
 
-studydir = '/Users/seunghyukyang/Desktop/Python/salicylate'
+studydir = '/Volumes/BOB_SAGET/TNFalpha/salicylate/Gap'
+freqs = np.array([5, 7.1, 10, 14.1, 20, 28.3, 40])
+ls = dict(salicylate = '--', control = '-')
 
-def combine_all():
+def combine_all(cage = ''):
 
-    fpaths = glob.glob(os.path.join(studydir, 'fileconversion', '*.h5'))
+    fpaths = glob.glob(os.path.join(studydir, 'fileconversion', '*%s*.h5' % cage))
     df = []
     for fpath in fpaths:
         absol, relat = os.path.split(fpath)
@@ -28,13 +30,12 @@ def combine_all():
         df.append(df_)
 
     df = pd.concat(df)
+    df.index = np.arange(len(df))
     
     return df
 
 def plot_indiv_results(df):
 
-    freqs = np.array([5, 7.1, 10, 14.1, 20, 28.3, 40])
-    colors = dict(salicylate = 'r', control = 'b')
     df = df[df.cued==1]
     gp = df.groupby('animalID')
     for animalID, v1 in gp:
@@ -45,10 +46,10 @@ def plot_indiv_results(df):
         for exp, v2 in gp1:
             gp2 = v2.groupby(['freq', 'sess'])
             gapratio = gp2.gapratio.apply(np.mean)
-            gapratio.unstack().plot(marker = '.', color = colors[exp], ax = ax)
+            gapratio.unstack().plot(marker = '.', linestyle = ls[exp], ax = ax)
             #gapratio.to_csv(os.path.join(studydir, 'forshaowen', '%s_%s.csv' % (animalID, exp)))
 
-        ax.legend()
+        ax.legend(loc = 'lower left')
         ax.set_ylim([0, 1.3])
         fig.savefig(os.path.join(studydir, 'Sheets', '_'.join([animalID, 'sheet'])))
         plt.close(fig);
@@ -60,10 +61,19 @@ def get_first(df):
     return x
 
 def plot_group_results():
-    
-<<<<<<< HEAD
-#def calc_gap_ratio():
-=======
+
+    df = df[df.cued==1]
+    gp_sess = df.groupby(['sess', 'freq'])
+    gapratio_sess = gp_sess.agg(dict(gapratio = np.mean, freq = get_first, gen = get_first, exp = get_first, animalID = get_first))
+    gp_animal = gapratio_sess.groupby(['animalID', 'freq'])
+    gapratio_animal = gp_animal.agg(dict(gapratio = np.mean, freq = get_first, gen = get_first, exp = get_first))
+    gp_exp = gapratio_animal.groupby(['exp', 'freq'])
+    gapratio_exp = gp_exp.gapratio.apply(np.mean)
+
+    plot(gapratio_exp)
+    pass    
+
+def calc_gap_ratio():
     df = df[df.cued==1]
     
     sess_group = df.groupby(['sess', 'freq'])
@@ -85,20 +95,19 @@ def plot_group_results():
     
     results.plot(kind = 'bar', ax = ax)
     return
->>>>>>> 89b8351b5d6f53abe8f4b580b335bbb9aeef84c8
 
 def add_gapratio(df):
      
-    if 'ampl' not in df.keys():
+    if 'startleampl' not in df.keys():
         df = add_amplitude(df)
 
-    uncued_amp = df.ampl[df.cued==0].mean()
-    df['gapratio'] = df.ampl / uncued_amp
+    uncued_amp = df.startleampl[df.cued==0].mean()
+    df['gapratio'] = df.startleampl / uncued_amp
     
     return df
     
 def add_amplitude(df):
-    df['ampl'] = find_amplitude(df)
+    df['startleampl'] = find_amplitude(df)
     return df
     
 def find_amplitude(df):
@@ -112,30 +121,27 @@ def find_amplitude(df):
     return resps
 
 def convert_to_pd_all():
-	fpaths = glob.glob(os.path.join(studydir, 'data', '*.txt'))
-	for fpath in fpaths:
-	    if not os.path.exists(fpath.replace('data', 'fileconversion').replace('.txt', '.h5')):
-                convert_to_pd(fpath)
+    fpaths = glob.glob(os.path.join(studydir, 'data', '*.txt'))
+    for fpath in fpaths:
+        convert_to_pd(fpath)
 
 def convert_to_pd(fpath):
 
-	header = np.loadtxt(fpath, 'S', delimiter = '\t')[0]
-	startletrace_ix = len(header)
-	x = np.loadtxt(fpath, 'f', skiprows = 1)
-	print header
-
-	freq = x[:, 0]
-	cued = x[:, 1]
-	ampl = x[:, 2]
-	holdtime = x[:, 4]
-	resp = [i for i in x[:, 5:]]
-	# resp = x[:, 5:]
-	df = pd.DataFrame(dict(freq = freq, cued = cued, ampl = ampl, holdtime = holdtime, resp = resp))
-
-	outpath = fpath.replace('data', 'fileconversion').replace('.txt', '.h5')
-	d = pd.HDFStore(outpath)
-	d['df'] = df
-	d.close()
+    outpath = fpath.replace('data', 'fileconversion').replace('.txt', '.h5')
+    if not os.path.exists(outpath):
+        print fpath
+        header = np.loadtxt(fpath, 'S', delimiter = '\t')[0]
+        startletrace_ix = len(header)
+        x = np.loadtxt(fpath, 'f', skiprows = 1)
+        freq = x[:, 0]
+        cued = x[:, 1]
+        ampl = x[:, 2]
+        holdtime = x[:, 4]
+        resp = [i for i in x[:, 5:]]
+        df = pd.DataFrame(dict(freq = freq, cued = cued, ampl = ampl, holdtime = holdtime, resp = resp))
+        d = pd.HDFStore(outpath)
+        d['df'] = df
+        d.close()
 
 def make_contact_sheet(df):
 
@@ -161,17 +167,17 @@ def calc_max_startle(gp):
 	return startle_amp
 
 
-# add KO and experiment type to file names
-#dates = dict(salicylate = ['5_30_2013', '6_3_2013', '6_4_2013'], control = ['5_23_2013', '5_31_2013', '6_1_2013'])
-#fpaths = glob.glob(os.path.join(studydir, 'fileconversion', '*.h5'))
-#for fpath in fpaths:
+# # add KO and experiment type to file names
+# dates = dict(salicylate = ['5_30_2013', '6_3_2013', '6_4_2013'], control = ['5_23_2013', '5_31_2013', '6_1_2013'])
+# fpaths = glob.glob(os.path.join(studydir, 'data', '*.txt'))
+# for fpath in fpaths:
 #    absol, relat = os.path.split(fpath)
 #    animalID, da, mo, yr, ext = relat.split('_')
 #    for k, v in dates.iteritems():
 #        for v_ in v:
 #            if v_ in relat:
 #                exp = k
-#                
+               
 #    newrelat = '_'.join([animalID, 'KO', exp, da, mo, yr, ext])
 #    shutil.move(fpath, os.path.join(absol, newrelat))
     
